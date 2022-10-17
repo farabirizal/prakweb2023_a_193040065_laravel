552e2da16e2e89e7b1d9e712bd06b607fcc55113
Rizal <farabirizal111@gmail.com>
2022-10-17 23:46:44 +0700

    membuat blade tamplating engine

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/home', function () {
-    return view('home');
+    return view('home',[
+        "tittle" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "tittle"=> "About",
         "name"=> "Rizal Muhammad Farabi",
         "email"=> "farabirizal111@gmail.com",
         "image"=> "me.jpg"
@@ -26,5 +29,7 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    return view('posts', [
+        "tittle"=>"Posts"
+    ]);
 });
