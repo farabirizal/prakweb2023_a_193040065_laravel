18ccfe8db1c4d7fb3584f696e2d5ae1fb62ee72d
Rizal <farabirizal111@gmail.com>
2022-10-24 15:03:16 +0700

    menyelesaikan single post

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -15,21 +15,69 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/home', function () {
     return view('home',[
-        "tittle" => "Home"
+        "title" => "Home"
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
-        "tittle"=> "About",
+        "title"=> "About",
         "name"=> "Rizal Muhammad Farabi",
         "email"=> "farabirizal111@gmail.com",
         "image"=> "me.jpg"
     ]);
 });
 
+
 Route::get('/blog', function () {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Rizal Muhammad Farabi",
+            "body"=> "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit architecto aliquam repellat assumenda alias minima at, tenetur excepturi enim, dolor voluptatem tempore quam deleniti. Consectetur aspernatur vitae officiis voluptate cum rerum, veniam quibusdam itaque dolore aperiam. Veritatis, atque! Obcaecati laborum odit tempora ex mollitia amet sit, distinctio, omnis corrupti sapiente, itaque unde quis! Dolore debitis culpa labore possimus distinctio perspiciatis vel error consectetur sapiente veritatis quibusdam commodi odit fugit vero illum voluptas, quam ex, in quo mollitia sunt est ut! Facilis tempora quaerat odio repellat asperiores? Libero, cum dolor animi nulla aspernatur culpa doloremque illo! Sapiente placeat inventore non molestiae?"
+        ],
+    
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Rizal Muhammad Farabi",
+            "body"=> "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit architecto aliquam repellat assumenda alias minima at, tenetur excepturi enim, dolor voluptatem tempore quam deleniti. Consectetur aspernatur vitae officiis voluptate cum rerum, veniam quibusdam itaque dolore aperiam. Veritatis, atque! Obcaecati laborum odit tempora ex mollitia amet sit, distinctio, omnis corrupti sapiente, itaque unde quis! Dolore debitis culpa labore possimus distinctio perspiciatis vel error consectetur sapiente veritatis quibusdam commodi odit fugit vero illum voluptas, quam ex, in quo mollitia sunt est ut! Facilis tempora quaerat odio repellat asperiores? Libero, cum dolor animi nulla aspernatur culpa doloremque illo! Sapiente placeat inventore non molestiae?"
+        ],
+    ];
+    
+    
     return view('posts', [
-        "tittle"=>"Posts"
+        "title"=>"Posts",
+        "posts"=> $blog_posts
+    ]);
+});
+
+Route::get('posts/{slug}', function($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Rizal Muhammad Farabi",
+            "body"=> "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit architecto aliquam repellat assumenda alias minima at, tenetur excepturi enim, dolor voluptatem tempore quam deleniti. Consectetur aspernatur vitae officiis voluptate cum rerum, veniam quibusdam itaque dolore aperiam. Veritatis, atque! Obcaecati laborum odit tempora ex mollitia amet sit, distinctio, omnis corrupti sapiente, itaque unde quis! Dolore debitis culpa labore possimus distinctio perspiciatis vel error consectetur sapiente veritatis quibusdam commodi odit fugit vero illum voluptas, quam ex, in quo mollitia sunt est ut! Facilis tempora quaerat odio repellat asperiores? Libero, cum dolor animi nulla aspernatur culpa doloremque illo! Sapiente placeat inventore non molestiae?"
+        ],
+    
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Rizal Muhammad Farabi",
+            "body"=> "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit architecto aliquam repellat assumenda alias minima at, tenetur excepturi enim, dolor voluptatem tempore quam deleniti. Consectetur aspernatur vitae officiis voluptate cum rerum, veniam quibusdam itaque dolore aperiam. Veritatis, atque! Obcaecati laborum odit tempora ex mollitia amet sit, distinctio, omnis corrupti sapiente, itaque unde quis! Dolore debitis culpa labore possimus distinctio perspiciatis vel error consectetur sapiente veritatis quibusdam commodi odit fugit vero illum voluptas, quam ex, in quo mollitia sunt est ut! Facilis tempora quaerat odio repellat asperiores? Libero, cum dolor animi nulla aspernatur culpa doloremque illo! Sapiente placeat inventore non molestiae?"
+        ],
+    ];
+$new_post = [];
+foreach($blog_posts as $post) {
+    if($post["slug"] === $slug) {
+        $new_post = $post;
+    }
+}
+
+    return view ('post',[
+        "title"=> "Single Post",
+        "post"=> $new_post
     ]);
 });
