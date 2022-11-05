bafae06dc15b4407c4dc0db9e6d881f15be42f63
Rizal <farabirizal111@gmail.com>
2022-11-05 23:07:35 +0700

    Video 14 Login & Registration View

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,9 +1,13 @@
 <?php
-use App\Models\Post;
+
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use App\Models\Category;
-use App\Models\User;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,14 +18,12 @@ use App\Models\User;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('home', [
         'title' => 'Home',
         'active' => 'home',
     ]);
 });
-
 Route::get('/about', function () {
     return view('about', [
         'title' => 'About',
@@ -31,11 +33,8 @@ Route::get('/about', function () {
         "image" => "me.jpg"
     ]);
 });
-
-
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('posts/{post:slug}',[PostController::class, 'show']);
-
 Route::get('/categories', function() {
     return view('categories', [
         'title' => 'Post Categories',
@@ -44,18 +43,5 @@ Route::get('/categories', function() {
     ]);
 });
 
-// Route::get('/categories/{category:slug}', function(Category $category){
-//     return view('posts', [
-//         'title' => "Post By Category : $category->name",
-//         'active' => 'categories',
-//         'posts' => $category->posts->load('category', 'author'),
-//     ]);
-// });
-
-// Route::get('/authors/{author:username}', function(User $author) {
-//     return view('posts', [
-//         'title' => "Post By Author : $author",
-//         'active' => 'posts',
-//         'posts' => $author->posts->load('category', 'author'),
-//     ]);
-// });
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
